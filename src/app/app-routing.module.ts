864d44d2f6bae79c323cfc53fbc4ff9a1935371e
DarioMzGz <dario@graywolf.co>
2022-05-10 03:04:36 -0500

    form templates added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InventarioComponent } from './components/inventario/inventario.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { ProveedoresComponent } from './components/proveedores/proveedores.component';
+import { FacturasComponent } from './components/facturas/facturas.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { ComprasComponent } from './components/compras/compras.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inventario', component: InventarioComponent},
+  {path: 'facturas', component: FacturasComponent},
+  {path: 'ventas', component: VentasComponent},
+  {path: 'proveedores', component: ProveedoresComponent},
+  {path: 'empleados', component: EmpleadosComponent},
+  {path: 'compras', component: ComprasComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'inventario' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventarioComponent } from './components/inventario/inventario.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { ProveedoresComponent } from './components/proveedores/proveedores.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { ComprasComponent } from './components/compras/compras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventarioComponent,
+    VentasComponent,
+    ProveedoresComponent,
+    NavbarComponent,
+    EmpleadosComponent,
+    ComprasComponent
   ],
   imports: [
     BrowserModule,
