dda23106a18ad37491630af863ff52002ea8ef72
DarioMzGz <dario@graywolf.co>
2022-05-23 00:28:06 -0500

    facturas se editan

--- src/app/components/facturas/facturas.component.ts
+++ src/app/components/facturas/facturas.component.ts
@@ -10,7 +10,10 @@ import { FacturasService } from '../../services/facturas.service';
 export class FacturasComponent implements OnInit {
 
   formFacturas:FormGroup;
+  formEditFacturas:FormGroup;
+
   facturas:any = null;
+  factura:any = null;
 
   constructor(private fb:FormBuilder,
               private facturasService:FacturasService) { }
@@ -27,12 +30,51 @@ export class FacturasComponent implements OnInit {
       rfc:[''],
       fecha:[''],
       iva:['']
+    });
+    this.formEditFacturas = this.fb.group({
+      id:[''],
+      nota:[''],
+      rfc:[''],
+      fecha:[''],
+      iva:['']
     })
   }
 
   getFacturas(){
     this.facturasService.getFacturas().subscribe(resultado => {
       this.facturas = resultado;
+    });
+  }
+
+  getFactura(id:number){
+    this.facturasService.getFactura(id).subscribe(resultado => {
+      this.factura = resultado;
+      this.formEditFacturas.setValue({
+        id:this.factura[0].id_factura,
+        nota:this.factura[0].nota,
+        fecha:this.factura[0].fecha_fac,
+        rfc:this.factura[0].rfc,
+        iva:this.factura[0].iva
+      });
+    });
+  }
+
+  editarFactura(id:number){
+    this.getFactura(id);
+  }
+
+  guardarEdicion(){
+    console.log(this.formEditFacturas.value);
+    this.facturasService.updateFactura(this.formEditFacturas.value).subscribe((datos:any) =>{
+      if (datos['resultado'] == 'OK') {
+        console.log(datos['resultado']);
+        this.getFacturas();
+        this.formEditFacturas.reset();
+        return;
+      }
+      if(datos['estatus'] == '1'){
+        alert("Ya existe una factura con este ID");
+      }
     });
   }
 
--- src/app/services/facturas.service.ts
+++ src/app/services/facturas.service.ts
@@ -16,8 +16,17 @@ export class FacturasService {
     return this.http.get(`${this.url}getFacturas.php`)
   }
 
+  getFactura(id:number){
+    return this.http.get(`${this.url}getFactura.php?id=${id}`)
+  }
+
   setFactura(factura:any){
     const FACTURA_FD = serialize(factura);
     return this.http.post(`${this.url}crearFactura.php`, FACTURA_FD).pipe(retry(3))
+  }
+
+  updateFactura(factura:any){
+    const FACTURA_FD = serialize(factura);
+    return this.http.post(`${this.url}updateFactura.php`, FACTURA_FD);
   }
 }
